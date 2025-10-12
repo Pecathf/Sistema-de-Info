feafab4a7864df13f33ae30ba2a748e12229c6c1
gloriafer2 <gloria.fernandes@correo.unimet.edu.ve>
2025-10-12 19:03:30 -0400

    Configuracion de firebase

---
+++ android/app/src/main/kotlin/com/example/sistem_proyect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sistem_proyect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/central/constantes/textos.dart
@@ -0,0 +1,9 @@
+class Textos {
+  // Título para la sección de proyectos en el Dashboard
+  static const String tituloProyectos = 'Gestión de Proyectos';
+  
+  // Título para la sección de perfil de usuario
+  static const String tituloPerfil = 'Mi Perfil';
+
+  // Podemos añadir más textos aquí a medida que crezca la aplicación.
+}
