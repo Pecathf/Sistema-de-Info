57202f47ab525949fb30f89950315b79d4d7bffa
Pepillo06 <jabrieliz.cuello@correo.unimet.edu.ve>
2025-10-15 21:50:13 -0400

    Pantalla Inicio
    Se agregó el código de la pantalla de inicio de sesión

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Repositorio-git-eduardo\Proyect App"
+export "FLUTTER_ROOT=C:\Users\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\PC\Desktop\hito2\Sistema-de-Info"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
