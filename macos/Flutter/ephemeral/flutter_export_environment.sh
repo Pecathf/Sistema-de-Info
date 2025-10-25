eaed0880d5f5dc0991942ff515afea1c20f9ab51
Pecathf <167771326+Pecathf@users.noreply.github.com>
2025-10-25 17:19:15 -0400

    Configuraciones extras, NO HACER MERGE

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
 export "FLUTTER_ROOT=C:\dev\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\Hito2\Sistema-de-Info"
-=======
-export "FLUTTER_ROOT=C:\Users\soyte\Downloads\flutter_windows_3.35.3-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\soyte\sistem_proyect"
->>>>>>> Gloria
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
